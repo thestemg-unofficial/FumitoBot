add13f48f7c77d2491b2b62e3406f59d07b98b0a
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-05-27 14:48:34 +0800

    build(flake): add lsp to build tools and update to sdk_7

--- flake.nix
+++ flake.nix
@@ -9,35 +9,51 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  
-  outputs = { self,
-              nixpkgs,
-              flake-utils,
-              nuget-packageslock2nix }: flake-utils.lib.eachDefaultSystem
-                (system: let
-                  
-                  pkgs = nixpkgs.legacyPackages.${system};
-         
-                  project = with pkgs; buildDotnetModule rec {
-                    pname = "fumitoBot";
-                    version = "0.1";
-                    src = ./.;
-                    projectFile = "${pname}.fsproj";
-                    packNupkg = true;
 
-                    nugetDeps = nuget-packageslock2nix.lib {
-                      system = system;
-                      lockfiles = [./packages.lock.json];
-                    };};
-                in
-                  {
-                    packages.${project.pname} = project;
-                    
-                    defaultPackage = self.packages.${system}.${project.pname};
-                    
-                    devShell = with pkgs; mkShell { buildInputs = [ dotnet-sdk ];
-                                                    shellHook = "dotnet restore";
-                                                  };
-                    
-                    inputsFrom = builtins.attrValues self.packages.${system};
-                  });}
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nuget-packageslock2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      dotnetPkg = with pkgs.dotnetCorePackages; combinePackages [sdk_7_0];
+      dotnetTools = pkgs.callPackage ./dotnet-tool.nix {};
+
+      project = with pkgs;
+        buildDotnetModule rec {
+          pname = "fumitoBot";
+          version = "0.1";
+          src = ./.;
+          projectFile = "${pname}.fsproj";
+          packNupkg = true;
+
+          nugetDeps = nuget-packageslock2nix.lib {
+            system = system;
+            lockfiles = [./packages.lock.json];
+          };
+        };
+    in {
+      packages.${project.pname} = project;
+
+      defaultPackage = self.packages.${system}.${project.pname};
+
+      devShell = with pkgs;
+        mkShell {
+          packages = [
+            dotnetPkg
+            (dotnetTools.combineTools
+              dotnetPkg
+              (with dotnetTools.tools; [
+                fsautocomplete
+              ]))
+          ];
+          shellHook = "dotnet restore";
+        };
+
+      inputsFrom = builtins.attrValues self.packages.${system};
+    });
+}
